a729be22dcd5ce300ff482ec80640d1b18e506e0
vasu <vasu@voxxup.com>
2023-07-04 23:30:44 +0530

    new

---
+++ lua/mkv/packer.lua
@@ -0,0 +1,53 @@
+vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+
+	}
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}	
+	use('nvim-treesitter/playground')
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'},
+		}
+	}
+	use {
+		"williamboman/mason.nvim",
+		run = ":MasonUpdate" -- :MasonUpdate updates registry contents
+	}
+	use('tpope/vim-commentary')
+
+	use('neovim/nvim-lspconfig')
+	use('jose-elias-alvarez/null-ls.nvim')
+	use('MunifTanjim/prettier.nvim')
+ use("eandrju/cellular-automaton.nvim")
+end)
---
+++ lua/mkv/remap.lua
@@ -0,0 +1,49 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv",vim.cmd.Ex)
+
+-- python excute with <leader>r
+vim.keymap.set('n', '<leader>rp', ":w<CR>:!python3 %<CR>")
+
+-- nodejs excute with <leader>rn
+vim.keymap.set('n', '<leader>rn', ":w<CR>:!node %<CR>")
+
+
+vim.keymap.set('n',"<leader>/", ":Commentary<cr>")
+vim.keymap.set('v',"<leader>/", ":Commentary<cr>")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>pp", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+
+
+vim.keymap.set('n',"<leader>]", ":><CR>")
+vim.keymap.set('n',"<leader>[", ":<<CR>")
+vim.keymap.set('v',"<leader>]", ":><CR>")
+vim.keymap.set('v',"<leader>[", ":<<CR>")
+
+vim.keymap.set('n',"<leader>wq", ":wq<CR>")
+vim.keymap.set('n',"<leader>q", ":q<CR>")
+
+
+-- replace selcted current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- move selected lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
+vim.keymap.set({'n','v'}, '<C-f>', "\"fy/\\<C-R>f<CR>")
+
+vim.keymap.set('n','<C-f>', ":noh<CR>")
+-- vim.keymap.set('n','<C-f>', '/<C-r><C-w>\>/<C-r><C-w>')
+
