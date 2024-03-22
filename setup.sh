3ae936eab67aa028e7a7182e8de8ec7c51a9c758
tamilanmkv <tamilanmkv@gmail.com>
2024-03-22 16:55:22 +0530

    setup file

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+#packer install
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 
 # check system linux or mac if its is windows get the hell out
 if [ "$(uname)" == "Darwin" ]; then
-    OS="mac"
-    
+    brew install neovim ripgrep
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    OS="linux"
+    sudo apt install neovim ripgrep -y
+
 fi
 
-if [ "$OS" == "mac" ]; then
-    echo "mac"
-elif [ "$OS" == "linux" ]; then
-    echo "linux"
-fi
+# backup old config file
+mv ~/.config/nvim/ ~/.config/nvim_old 2&>/dev/null
+ln -s $(pwd) ~/.config/nvim
+
+
+# compile nvim lua plugin
+nvim ~/.config/nvim/lua/mkv/packer.lua -c 'so' -c 'autocmd User PackerComplete quitall' -c 'PackerSync' --headless 2&>/dev/null
+ 
