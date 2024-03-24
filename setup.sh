ba812a7d640e52e57f71dc48a2c28cf730822441
tamilanmkv <tamilanmkv@gmail.com>
2024-03-24 23:20:02 +0530

    readme update

--- setup.sh
+++ setup.sh
@@ -4,12 +4,15 @@
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim 2&>/dev/null
 
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
 # check system linux or mac if its is windows get the hell out
 if [ "$(uname)" == "Darwin" ]; then
     brew install neovim ripgrep fzf
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    sudo apt install neovim ripgrep fzf -y
+    sudo apt install ripgrep fzf -y
 
 fi
 
